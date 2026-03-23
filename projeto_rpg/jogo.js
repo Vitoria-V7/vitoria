de3b6d012b409ac739804b2b4caa673ed6e015fb
Liwott <vitoriaalves10193@gmail.com>
2026-03-23 12:01:06 -0300

    Eventos

--- projeto_rpg/jogo.js
+++ projeto_rpg/jogo.js
@@ -6,10 +6,33 @@ class Personagem {
         this.mana = mana;
         this.energia = energia;
     }
-    atacar(alvo, habilodade) {
+    atacar(alvo, habilidade) {
+        //verificar ser tem mana
+        if (this.mana >= habilidade.custo && this.energia >= habilidade.energia) {
+            alvo.hp -= habilidade.dano;
+            //Depositar mana 
+            if (habilidade.custo > 0) {
+                this.mana -= habilidade.custo;
+                this.energia += 50;
+            }
+            if (habilidade.energia > 0) {
+                this.energia = 0;
+            }
+            // retornar mensagens
+            return `${this.nome} usou ${habilidade.nome}`;
+        } else {
+            return `Sem mana ou energia para usar para usar &{habilidade.nome}`;
+        }
+    }
+    boss_atacar(alvo) {
+        if (this.energia == 100) {
+            alvo.hp -= 15;
+            this.energia = 0;
+        } else {
+            this.energia += 50;
+        }
 
     }
-}
 
 class Habilidade {
     constructor(id, nome, dano, custo, energia) {
@@ -22,18 +45,32 @@ class Habilidade {
 
 }
 // criar os objetos
-let hero = new Personagem("David", "O Heroi", 100, 100, 0);
-let boss = new Personagem("Smaug", "Elden Beast", 100, 0, 50);
+let hero = new Personagem("David", "🗡️", 100, 100, 0);
+let boss = new Personagem("Smaug", "🔥", 100, 0, 50);
 
 // Prencher Status
-document.getElementById("nome-heroi").textContent = `🗡️${hero.nome}`;
-document.getElementById("titulo-heroi").textContent = `🗡️${hero.titulo}`;
-document.getElementById("nome-boss").textContent = `🔥${boss.nome}`;
-document.getElementById("titulo-boss").textContent = `🔥${boss.titulo}`;
+document.getElementById("nome-heroi").textContent = `${hero.nome}`;
+document.getElementById("titulo-heroi").textContent = `${hero.titulo}`;
+document.getElementById("nome-boss").textContent = `${boss.nome}`;
+document.getElementById("titulo-boss").textContent = `${boss.titulo}`;
+
+//atualizar as barras de vida 
+const atualizarInterface = (mensagem) => {
+    console.log(hero.mana)
+
+    // barra heroi
+    document.getElementById("hp-hero").value = hero.hp;
+    document.getElementById("mp-hero").value = hero.mana;
+    document.getElementById("en-hero").value = hero.energia;
+
+
+
+
+}
 
 
 //criar habilidade
-let container = document.getElementById("controle");
+let container = document.getElementById("controles");
 let listaHabilidades = [
     new Habilidade(1, "attack", 4, 0,),
     new Habilidade(2, "skill", 8, 10, 0),
@@ -42,7 +79,13 @@ let listaHabilidades = [
 listaHabilidades.forEach(hab => {
     let btn = document.createElement("button");
     btn.innerText = hab.nome;
-    btn.classList.add("btn","btn-warning");
+    btn.classList.add("btn", "btn-warning");
     container.appendChild(btn);
+    btn.onclick = () => {
+
+        let mensagem = hero.atacar(boss, hab);
+        atualizarInterface(mensagem);
+        boss.boss_atacar(hero);
+    }
 });
 
